20a36dfb896bee927673e7f8613d421d27192d0a
samurai3452345 <magomeggaziev4@gmail.com>
2026-06-14 23:05:04 +0300

    Initial commit

---
+++ src/test/java/com/java_dragons/dnd_tenebres/DnDTenebresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_dragons.dnd_tenebres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnDTenebresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
