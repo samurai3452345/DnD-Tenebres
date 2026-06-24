8a8b9a3df35333f1f1363f28bde01de6d7e610d3
Sawaron <shoma111125@gmail.com>
2026-06-24 15:34:19 +0300

    add folder exception and dto add playerService and PlayerRepository add createPlayer method in service

---
+++ src/main/java/com/java_dragons/dnd_tenebres/player/dto/PlayerCreationRequest.java
@@ -0,0 +1,6 @@
+package com.java_dragons.dnd_tenebres.player.dto;
+
+
+public record PlayerCreationRequest(String name , int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma) {
+
+}
