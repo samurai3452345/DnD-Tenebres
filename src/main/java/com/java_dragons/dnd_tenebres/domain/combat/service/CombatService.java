109fdfbbf065d262a0dd743fa634dd2be660d015
samurai3452345 <magomeggaziev4@gmail.com>
2026-07-03 00:16:05 +0300

    del: MonsterMock class

--- src/main/java/com/java_dragons/dnd_tenebres/domain/combat/service/CombatService.java
+++ src/main/java/com/java_dragons/dnd_tenebres/domain/combat/service/CombatService.java
@@ -1,9 +1,9 @@
 package com.java_dragons.dnd_tenebres.domain.combat.service;
 
 import com.java_dragons.dnd_tenebres.domain.item.entity.Weapon;
-import com.java_dragons.dnd_tenebres.domain.monster.entity.MonsterMock;
+import com.java_dragons.dnd_tenebres.domain.monster.entity.Monster;
 import com.java_dragons.dnd_tenebres.domain.player.entity.Player;
 
 public interface CombatService {
-    String executeAttack(Player attacker, Weapon weapon, MonsterMock defender);
+    String executeAttack(Player attacker, Weapon weapon, Monster defender);
 }
