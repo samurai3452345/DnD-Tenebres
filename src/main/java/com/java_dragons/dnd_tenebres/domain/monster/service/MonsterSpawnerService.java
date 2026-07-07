71d63cc326234d7c4e3caa5df546444adaa18d0d
Sawaron <shoma111125@gmail.com>
2026-07-08 00:16:27 +0300

    change monsterspawnerservice fot the better

--- src/main/java/com/java_dragons/dnd_tenebres/domain/monster/service/MonsterSpawnerService.java
+++ src/main/java/com/java_dragons/dnd_tenebres/domain/monster/service/MonsterSpawnerService.java
@@ -1,40 +1,43 @@
 package com.java_dragons.dnd_tenebres.domain.monster.service;
 
-import com.java_dragons.dnd_tenebres.domain.combat.model.DamageType;
+
 import com.java_dragons.dnd_tenebres.domain.monster.entity.Monster;
+import com.java_dragons.dnd_tenebres.domain.monster.entity.MonsterTemplate;
 import com.java_dragons.dnd_tenebres.domain.monster.repository.MonsterRepository;
+import com.java_dragons.dnd_tenebres.domain.monster.repository.MonsterTemplateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 @RequiredArgsConstructor
 public class MonsterSpawnerService {
     private final MonsterRepository monsterRepository;
-
-
-
-    private static final List<String> MONSTER_NAMES = List.of(
-            "Дикий волк", "Скелет", "Гоблин", "Орк", "Зомби"
-    );
+    private final MonsterTemplateRepository monsterTemplateRepository;
 
     @Transactional
-    public Monster spawnRandomMonster(int playerLevel){
-        int randomIndex = java.util.concurrent.ThreadLocalRandom.current().nextInt(MONSTER_NAMES.size());
+    public Monster spawnRandomMonster(String biome, int locationLevel) {
+        List<MonsterTemplate> templates =
+                monsterTemplateRepository.findAllByBiomeAndLevel(biome, locationLevel);
+        if(templates.isEmpty()) {
+            throw new IllegalArgumentException("Нет шаблонов монстров для данного биома и уровня!");
+        }
 
-        String name = MONSTER_NAMES.get(randomIndex);
-        int maxHp = 20 + (playerLevel * 5);
-        int armorClass = 10 + (playerLevel / 2);
-        int xpReward = playerLevel + 15;
-        int goldReward = playerLevel + 10;
-        Set<DamageType> elements = new HashSet<>(Set.of(DamageType.PHYSICAL));
-        Monster monster = new Monster(name, maxHp, armorClass, xpReward, goldReward, elements);
+       MonsterTemplate monsterTemplate = templates.get(ThreadLocalRandom.current().nextInt(templates.size()));
 
+        Monster monster = new  Monster(
+                monsterTemplate.getName(),
+                monsterTemplate.getBaseHp(),
+                monsterTemplate.getArmorClass(),
+                monsterTemplate.getXpReward(),
+                monsterTemplate.getGoldReward(),
+                new HashSet<>(monsterTemplate.getElements())
+        );
 
-        return  monsterRepository.save(monster);
+        return monsterRepository.save(monster);
     }
 }
