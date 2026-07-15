f700bd7a9eb0adebaf60203eb2f0bd64ae702fae
Sawaron <shoma111125@gmail.com>
2026-07-16 01:31:45 +0300

    add entity PlayerItem

---
+++ src/main/java/com/java_dragons/dnd_tenebres/domain/item/entity/PlayerItem.java
@@ -0,0 +1,51 @@
+package com.java_dragons.dnd_tenebres.domain.item.entity;
+
+import com.java_dragons.dnd_tenebres.domain.player.entity.Player;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "player_items")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PlayerItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "player_id", nullable = false)
+    private Player player;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "template_id", nullable = false)
+    private ItemTemplate template;
+
+    @Column(name = "amount", nullable = false)
+    private int amount;
+
+    @Column(name = "is_equipped", nullable = false)
+    private boolean isEquipped;
+
+    @Column(name = "bonus_strength", nullable = false)
+    private int bonusStrength;
+
+    @Column(name = "bonus_dexterity", nullable = false)
+    private int bonusDexterity;
+
+    @Column(name = "bonus_constitution", nullable = false)
+    private int bonusConstitution;
+
+    @Column(name = "bonus_intelligence", nullable = false)
+    private int bonusIntelligence;
+
+    @Column(name = "bonus_wisdom", nullable = false)
+    private int bonusWisdom;
+
+    @Column(name = "bonus_charisma", nullable = false)
+    private int bonusCharisma;
+}
