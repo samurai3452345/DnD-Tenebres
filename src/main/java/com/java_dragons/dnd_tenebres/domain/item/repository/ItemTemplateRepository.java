75ef8ff75cdef3c5f9c2fa7f3185a3be653ffddc
Sawaron <shoma111125@gmail.com>
2026-07-16 01:32:17 +0300

    add repo ItemTemplateRepository

---
+++ src/main/java/com/java_dragons/dnd_tenebres/domain/item/repository/ItemTemplateRepository.java
@@ -0,0 +1,10 @@
+package com.java_dragons.dnd_tenebres.domain.item.repository;
+
+import com.java_dragons.dnd_tenebres.domain.item.entity.ItemTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ItemTemplateRepository extends JpaRepository<ItemTemplate, Long> {
+    Optional<ItemTemplate> findByName(String name);
+}
