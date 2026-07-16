0e2353cc4dcd25771785bebd79d74be3a20d401e
Sawaron <shoma111125@gmail.com>
2026-07-16 11:23:17 +0300

    change modificator ItemTemplateRepository

--- src/main/java/com/java_dragons/dnd_tenebres/domain/item/repository/ItemTemplateRepository.java
+++ src/main/java/com/java_dragons/dnd_tenebres/domain/item/repository/ItemTemplateRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface ItemTemplateRepository extends JpaRepository<ItemTemplate, Long> {
+interface ItemTemplateRepository extends JpaRepository<ItemTemplate, Long> {
     Optional<ItemTemplate> findByName(String name);
 }
